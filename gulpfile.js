a80119b1414722c92ce021d69e43f2c3571cc995
Joseph Le <josephle@me.com>
2015-07-08 13:01:26 -0700

    Set up elixir with Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 var elixir = require('laravel-elixir');
 
+var paths = {
+ 'bootstrap': './bower_components/bootstrap-sass/assets/',
+ 'jquery': './bower_components/jquery/'
+};
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we are compiling the Less
  | file for our application, as well as publishing vendor resources.
  |
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+ mix.sass('app.scss', 'public/css/',
+   {
+    includePaths: [
+     paths.bootstrap + 'stylesheets/'
+    ]
+   })
+   .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+   .scripts([
+    paths.jquery + "dist/jquery.js",
+    paths.bootstrap + "javascripts/bootstrap.js",
+    './resources/assets/js/**/*.js'
+   ], 'public/js/app.js', './')
+   .version(['css/app.css', 'js/app.js']);
 });
