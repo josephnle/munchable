4427315ff2efdd694eb150c644313733ece11d42
kim-vu <kim.vu@cbsinteractive.com>
2015-07-08 21:33:15 -0700

    logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -29,7 +29,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand logo" href="#">Munchable</a>
+            <a class="navbar-brand logo" href="/">Munchable</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
